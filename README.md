6c5b0916ed661e6ee997688943d5f088c8f228ec
jiaxiang cheng <jiaxiangcheng7@gmail.com>
2022-04-15 17:14:28 +0200

    repo updated

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 # restaapi
 
-- dotevc is a package that help us to hide some information from our code if we push on any platform. In this case in .env we have the information where we want to hide from our code.
+-   dotevc is a package that help us to hide some information from our code if we push on any platform. In this case in .env we have the information where we want to hide from our code.
 
-- To execute the server we use: 'npm start' and then we can go to the localhost:PORT.
+-   To execute the server we use: 'npm start' and then we can go to the localhost:PORT.
+
+-   'git push heroku master' to deploy the app to heroku
--- app.js
+++ app.js
@@ -1,45 +1,33 @@
+// Libraries
 const express = require("express");
-const app = express();
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 const cors = require("cors");
+require("dotenv/config"); // read data from .env
 
-require("dotenv/config");   // read data from .env
-
-// 每一次我们接受到一个reques 都会从这个点运行所有以下的function
-
+// Creating the app
+const app = express();
 app.use(cors());
-app.use(bodyParser.json())  // we make sure body parser works
+app.use(bodyParser.json()); // we make sure body parser works
 
-// Import Routes
+// Creating Middlewares (Routes)
 const licensesRoute = require("./routes/licenses");
 const availableKeysRoute = require("./routes/availablekeys");
+
+// Applying Middlewares
 app.use("/licenses", licensesRoute);
 app.use("/availablekeys", availableKeysRoute);
 
 app.get("/", (req, res) => {
-    res.send("we are at home");
+	res.send("api running...");
 });
- 
-/*
-const License = require("./models/License");
-app.get("/licenses", async (req, res) => {
-    console.log("asdasdasdsad");
-    try {
-        const licenses = await License.find();
-        console.log(licenses);
-        res.json(licenses);
-    } catch (error) {
-        res.json({message: error});
-    }
-});*/
 
 // Connect to DB
 mongoose.connect(
-    process.env.DB_CONNECTION,
-    { useNewUrlParser: true },
-    () => console.log("Conected to db!")
+	process.env.DB_CONNECTION,
+	{ useNewUrlParser: true, useUnifiedTopology: true },
+	() => console.log("Conected to db!")
 );
 
-// Start listsening to the server
+// Start listsening to the enpoint http://localhost:3000/
 app.listen(process.env.PORT || 3000);
